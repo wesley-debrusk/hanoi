fe60b817e60ce98570873b83281d0a77a77c40aa
wesley-debrusk <wesdebrusk@gmail.com>
2020-03-12 20:55:54 -0400

    restructure

--- Game.py
+++ Game.py
@@ -5,22 +5,25 @@ class Hanoi:
 	def __init__(self, rings):
 		self.numRings = rings
 		self.towers = 3
-		self.rings = []
 
 		self.tower1 = []
 		self.tower2 = []
 		self.tower3 = []
 
-		for x in range(self.numRings-1, -1, -1):
-			self.rings.append(x + 1)
-
 		for x in range(self.numRings):
-			self.tower1.append(self.rings[x])
+			self.tower1.append(x + 1)
+			self.tower2.append(0)
 
 
 	def print_board(self):
 		for x in range(self.numRings):
 			print("   |      |      |   ")
-			print("   " + str(self.tower1.pop()));
+			print("   " + display(self.tower1[x]));
 		print("   |      |      |   ")
 		print("_____________________")
+
+def display(num):
+	if (num == 0):
+		return "|"
+	else:
+		return str(num)
